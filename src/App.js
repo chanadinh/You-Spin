cd86983582a9871f3b850fcd218f0d21af128022
Chan Dinh <162244788+chanadinh@users.noreply.github.com>
2024-12-01 22:27:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const App = () => {
   setHasPhoto(true);
 
     try {
-      const response = await fetch("https://3h8mnwen69.execute-api.us-east-1.amazonaws.com/2", {
+      const response = await fetch("http://127.0.0.1:5000/process-image", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
